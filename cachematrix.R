89d6debe8920802888cd13eed6247733e53b980c
Kelly Brew <samuel_brew@yahoo.com>
2014-11-21 18:18:25 +0000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,39 @@
 ## The purpose of writing this fucntion is to cache potentially
 ## time-consuming computations
 ## The function makeCacheMatrix contains the following functions:
-## * setMat       set the value of the matrix
-## * getMat       get the value of the matrix
-## * getCache     get the cached value
-## * getInverse   get the cached value (inverse of the matrix)
+## * setMat      set the value of the matrix
+## * getMat      get the value of the matrix
+## * getCache    get the cached value
+## * getInverse  get the cached value (inverse of the matrix)
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+
+  # holds the cached value or NULL if nothing is cached
+  # initially nothing is cached so set it to NULL
+  cache <- NULL
+
+  # store a matrix
+  setMat <- function(newValue) {
+    x <<- newValue
+    # since the matrix is assigned a new value, flush the cache
+    cache <<- NULL
+  }
+
+  # returns the stored matrix
+  getMat <- function() {
+    x
+  }
+
+  # cache the given argument
+  getCache <- function(solve) {
+    cache <<- solve
+  }
+
+  # get the cached value
+  getInverse <- function() {
+    cache
+  }
 
 }
 
@@ -18,4 +44,22 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+  inverse <- y$getInverse()
+  # if a cached value exists return it
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  # otherwise get the matrix, caclulate the inverse and store it in
+  # the cache
+  data <- y$getMat()
+  inverse <- solve(data)
+  y$getCache(inverse)
+
+  # return the inverse
+  inverse
+}
+
+
 }
